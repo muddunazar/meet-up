b76ea76d1dbee72186cda749b5a0c923c7054244
muddunazar <mudassarnazar3010@gmail.com>
2024-05-14 17:59:25 +0530

    added p tag

--- components/meetups/MeetupList.js
+++ components/meetups/MeetupList.js
@@ -1,20 +1,21 @@
-import MeetupItem from './MeetupItem';
-import classes from './MeetupList.module.css';
+import MeetupItem from "./MeetupItem";
+import classes from "./MeetupList.module.css";
 
 function MeetupList(props) {
-  return (
-    <ul className={classes.list}>
-      {props.meetups.map((meetup) => (
-        <MeetupItem
-          key={meetup.id}
-          id={meetup.id}
-          image={meetup.image}
-          title={meetup.title}
-          address={meetup.address}
-        />
-      ))}
-    </ul>
-  );
+ return (
+  <ul className={classes.list}>
+   <p
+    style={{ color: typeof window == "undefined" ? "red" : "green" }}
+    className={`${typeof window == "undefined" ? "trueCodition" : "falseCodition"}`}
+   >
+    Welcome to sunlighten customers care
+    {`${typeof window == "undefined" ? "trueCodition" : "falseCodition"}`}
+   </p>
+   {props.meetups.map((meetup) => (
+    <MeetupItem key={meetup.id} id={meetup.id} image={meetup.image} title={meetup.title} address={meetup.address} />
+   ))}
+  </ul>
+ );
 }
 
 export default MeetupList;
